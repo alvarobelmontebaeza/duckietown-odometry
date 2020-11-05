b5bd4093b9420a1f20353e04b6fb33dbe370a4e3
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2020-11-05 11:23:47 +0100

    Fixed distance traveled by accounting initial encoder ticks

--- packages/duckietown-odometry/src/duckietown-odometry.py
+++ packages/duckietown-odometry/src/duckietown-odometry.py
@@ -15,14 +15,19 @@ class OdometryNode(DTROS):
 
         # Initialize the DTROS parent class
         super(OdometryNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
-        self.veh_name = rospy.get_namespace().strip("/")
+        self.veh_name = "/" + os.environ['VEHICLE_NAME']
         self.left_distance = 0.0
         self.prev_left = 0.0
         self.right_distance = 0.0
         self.prev_right = 0.0
+        self.first_message_left = True
+        self.first_message_right = True
+        self.initial_ticks_left = 0.0
+        self.initial_ticks_right = 0.0
 
         # Get static parameters
         self._radius = rospy.get_param(f'{self.veh_name}/kinematics_node/radius', 100)
+        self._resolution = 135
 
         # Subscribing to the wheel encoders
         self.sub_encoder_ticks_left = rospy.Subscriber(f'{self.veh_name}/left_wheel_encoder_node/tick',WheelEncoderStamped,callback=self.cb_encoder_data,callback_args='left')
@@ -40,20 +45,35 @@ class OdometryNode(DTROS):
         """
         # Retrieve encoder data
         ticks = msg.data
-        resolution = msg.resolution
 
-        # Compute total distance measured by the encoder
-        total_dist = (2 * np.pi * self._radius) * (ticks/resolution)
+        # Check if it's the first received message and store initial encoder value
+        if wheel == 'left' and self.first_message_left == True:
+            self.first_message_left = False
+            self.initial_ticks_left = ticks
+        if wheel == 'right' and self.first_message_right ==True:
+            self.first_message_right = False
+            self.initial_ticks_right = ticks
+
+        # Compute total distance traveled by the wheel       
 
         if wheel == 'left':
-            self.left_distance += total_dist - self.prev_left
+            rel_ticks = ticks - self.initial_ticks_left
+            diff_ticks = np.abs(rel_ticks - self.prev_left)
+            dist = (2 * np.pi * self._radius) * (diff_ticks/self._resolution)
+            # Accumulate distance and publish it
+            self.left_distance += dist
             self.pub_integrated_distance_left.publish(self.left_distance)
-            self.prev_left = total_dist
+            self.prev_left = rel_ticks
+            self.log(self.left_distance)
 
         elif wheel == 'right':
-            self.right_distance += total_dist - self.prev_right
+            rel_ticks = ticks - self.initial_ticks_right
+            diff_ticks = np.abs(rel_ticks - self.prev_right)
+            dist = (2 * np.pi * self._radius) * (diff_ticks/self._resolution)
+            # Accumulate distance and publish it
+            self.right_distance += dist
             self.pub_integrated_distance_right.publish(self.right_distance)
-            self.prev_right = total_dist
+            self.prev_right = rel_ticks
 
 
     def cb_executed_commands(self, msg):
@@ -64,5 +84,5 @@ class OdometryNode(DTROS):
 if __name__ == '__main__':
     node = OdometryNode(node_name='my_odometry_node')
     # Keep it spinning to keep the node alive
-    rospy.spin()
     rospy.loginfo("wheel_encoder_node is up and running...")
+    rospy.spin()
